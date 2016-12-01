86b1d8f4fdc142b46d564c9009f8d0f583d90997
Y.Akamatsu <E87797@jp.ibm.com>
2016-12-01 10:54:22 +0900

    日付をフォーマット

--- src/main/java/com/ibm/jp/sample/controller/DatabaseSysdateServlet.java
+++ src/main/java/com/ibm/jp/sample/controller/DatabaseSysdateServlet.java
@@ -3,8 +3,10 @@ package com.ibm.jp.sample.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,6 +20,7 @@ import com.ibm.jp.sample.model.DatabaseSysdateModel;
 @WebServlet("/dbSysdate")
 public class DatabaseSysdateServlet extends DemoBaseServlet {
 	private static final long serialVersionUID = 1L;
+	private SimpleDateFormat sdf;
 
 	/**
 	 * @see DemoBaseServlet#DemoBaseServlet()
@@ -25,6 +28,12 @@ public class DatabaseSysdateServlet extends DemoBaseServlet {
 	public DatabaseSysdateServlet() {
 		super();
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		sdf = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss.SSS zZ");
 	}
 
 	/**
@@ -36,10 +45,11 @@ public class DatabaseSysdateServlet extends DemoBaseServlet {
 		Date date = null;
 		try {
 			date = DatabaseSysdateModel.getSysdate(dataSource);
+			String formatedDate = sdf.format(date);
+			request.setAttribute("bean", formatedDate);
 		} catch (SQLException e) {
 			throw new ServletException(e);
 		}
-		request.setAttribute("bean", date);
 		rendar(request, response);
 	}
 
@@ -56,10 +66,10 @@ public class DatabaseSysdateServlet extends DemoBaseServlet {
 		setResponseHeaders(response);
 		PrintWriter pw = response.getWriter();
 		pw.println("<html><head><title>Database System date</title></head>");
-		pw.print("<body><br><br>");
+		pw.print("<body><br><br><b>");
 		Object obj = request.getAttribute("bean");
 		pw.print(obj != null ? obj.toString() : "null");
-		pw.println("<br><br><a href=\"javascript:history.back();\">Back to Index</a>");
+		pw.println("</b><br><br><a href=\"javascript:history.back();\">Back to Index</a>");
 		pw.println("</body></html>");
 		pw.flush();
 		pw.close();
